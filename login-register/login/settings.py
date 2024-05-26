2c37c9bd47aec253e74df67cd2dc0a52bc637639
JavierLuceroD <javier.lucero.d@gmail.com>
2024-05-26 16:40:54 -0400

    1.4

--- login-register/login/settings.py
+++ login-register/login/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR=os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -132,3 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # VARIABLES DE REDIRECCION DE LOGIN Y LOGOUT
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+STAIC_URL = '/static/'
+STATICFILES_DIRS =[STATIC_DIR,]
